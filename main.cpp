2fd578b6bf1b8f68b092afee61539aa099b3081e
Tetiana Perederii <lapshun.tatiana@gmail.com>
2026-03-15 10:02:30 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include "DArrayTemplate.h"
+
+int main() {
+
+    int row = 0, col = 0;
+    cout << "input row: "; cin >> row;
+    cout << "input col: " ; cin >> col;
+
+    int** m = nullptr;
+
+
+    return 0;
+
+}
