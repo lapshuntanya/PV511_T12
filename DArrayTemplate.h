58bb0dda606f2f2bbbf9e0407c4ed2a1f1ef1c24
Tetiana Perederii <lapshun.tatiana@gmail.com>
2026-03-15 10:19:43 +0200

    create

--- DArrayTemplate.h
+++ DArrayTemplate.h
@@ -4,13 +4,24 @@ using namespace std;
 
 template <typename T>
 void createMemory(T** &matr, int row, int col) {
-
     //Створюємо масив покажчиків
-    matr = new T*[row];
+    matr = new T *[row];
 
-    for ( int i =0; i < row; i++) {
+    for (int i = 0; i < row; i++) {
         //Під кожний покажчик створюємо масив значень
-        matr[i] =new T[col] {0};
+        matr[i] = new T[col]{0};
     }
+}
 
+
+template <typename T>
+void destroyMemory(T** &matr, int &row, int &col) {
+    //видаляємо масиви значень
+    for (int i = 0; i < row; i++) {
+        delete[] matr[i];
+    }
+    //видаляємо масив покажчиків
+    delete[] matr;
+    matr = nullptr;
+    row = col = 0;
 }
