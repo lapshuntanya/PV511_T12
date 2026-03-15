b8c714c37134dfd0c36fb5a918f9cc5488ee15ff
Tetiana Perederii <lapshun.tatiana@gmail.com>
2026-03-15 10:15:50 +0200

    create

--- DArrayTemplate.h
+++ DArrayTemplate.h
@@ -1,3 +1,15 @@
 #pragma once
 #include <iostream>
 using namespace std;
+
+template <typename T>
+void createMemory(T** &matr, int row, int col) {
+
+    //Створюємо масив покажчиків
+    matr = new T*[row];
+
+    for ( int i =0; i < row; i++) {
+        //Під кожний покажчик створюємо масив значень
+        matr[i] =new T[col] {0};
+    }
+}
